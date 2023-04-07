cd8729fe12c26537677589437f0cbd190ac101c6
lijinghao <lijinghao@buaa.edu.com>
2023-04-07 10:59:49 +0800

    feat: 词单初始布局

---
+++ src/components/wordList/WordCardList.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="container">
+    <n-card class="card" :bordered="false" hoverable>
+      <template #header>
+        <div class="head">
+          <n-text class="font-color" v-text="'dangerous'"/>
+        </div>
+      </template>
+      <template #header-extra>
+        <n-text class="font-color" v-text="'【deng3eres】'"/>
+      </template>
+      <div class="content">
+        <n-text class="font-color" v-text="'adj. 有危险的'"/>
+      </div>
+    </n-card>
+    <n-card class="card" :bordered="false" hoverable>
+      <template #header>
+        <div class="head">
+          <n-text class="font-color" v-text="'dangerous'"/>
+        </div>
+      </template>
+      <template #header-extra>
+        <n-text class="font-color" v-text="'【deng3eres】'"/>
+      </template>
+      <div class="content">
+        <n-text class="font-color" v-text="'adj. 有危险的'"/>
+      </div>
+    </n-card>
+    <n-card class="card" :bordered="false" hoverable>
+      <template #header>
+        <div class="head">
+          <n-text class="font-color" v-text="'dangerous'"/>
+        </div>
+      </template>
+      <template #header-extra>
+        <n-text class="font-color" v-text="'【deng3eres】'"/>
+      </template>
+      <div class="content">
+        <n-text class="font-color" v-text="'adj. 有危险的'"/>
+      </div>
+    </n-card>
+  </div>
+
+</template>
+
+<script>
+import {NCard} from "naive-ui";
+
+export default {
+  name: "WordCardList",
+  components: {
+    NCard
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  background-color: #76ff73;
+  padding: 10px;
+  border-radius: 25px;
+}
+
+.card {
+  background-color: transparent;
+}
+
+.font-color {
+  color: white;
+}
+
+.head {
+  text-align: left;
+  font-size: 2em;
+}
+
+.content {
+  text-align: left;
+}
+</style>
