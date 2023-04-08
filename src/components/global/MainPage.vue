e6adff0c407a923f7c3e388dae5b7e528118ac8d
lijinghao <lijinghao@buaa.edu.com>
2023-04-08 15:45:48 +0800

    feat: 初始化数据统计界面和复习界面（未布局）

---
+++ src/components/global/MainPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <SideBar/>
+  <router-view/>
+</template>
+
+<script>
+import SideBar from "@/components/global/SideBar.vue";
+
+export default {
+  name: "MainPage",
+  components: {
+    SideBar
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/statistic/StatisticPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    这是数据统计页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StatisticPage"
+}
+</script>
+
+<style scoped>
+
+</style>
