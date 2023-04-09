d4fb2e755afc55e7e9c3e02c14d71bb42f333ff6
lijinghao <lijinghao@buaa.edu.com>
2023-04-09 17:33:28 +0800

    feat: 数据统计页面添加折线图

--- src/components/statistic/StatisticPage.vue
+++ src/components/statistic/StatisticPage.vue
@@ -2,25 +2,25 @@
   <div class="container">
     <div style="width: 50%">
       <!--      当日数据（今日数据）-->
-      <div>
+      <div style="">
         <n-row>
           <n-col :span="12">
-            <n-statistic style="" :value="' / '">
+            <n-statistic :value="' / '">
               <template #label>
                 <span style="color: black">
                   今日新学/今日复习
                 </span>
               </template>
               <template #prefix>
-                98(新学单词数)
+                98
               </template>
               <template #suffix>
-                23(复习单词数)
+                23
               </template>
             </n-statistic>
           </n-col>
           <n-col :span="12">
-            <n-statistic label="">
+            <n-statistic>
               <template #label>
                 <span style="color: black">
                   今日学习时长
@@ -35,18 +35,25 @@
         </n-row>
       </div>
       <!--      图表展示-->
-      <div>
-
-      </div>
+      <div class="chart" ref="chartRef"></div>
     </div>
     <div class="calendar">
       <n-calendar
           style="width: 100%;height: 100%"
           v-model:value="value"
-          #="{ year, month, date }"
           :is-date-disabled="isDateDisabled"
           @update:value="handleUpdateValue"
       >
+        <!--        日期框内内容-->
+        <template #default="{year, month, date}">
+          <span v-if="calendarData[year+'-'+month+'-'+date]">
+          {{ calendarData[year + '-' + month + '-' + date]['new'] }}/{{ calendarData[year + '-' + month + '-' + date]['old'] }}
+          </span>
+        </template>
+        <!--        年月-->
+        <template #header="{year, month}">
+
+        </template>
       </n-calendar>
     </div>
 
@@ -54,14 +61,44 @@
 </template>
 
 <script>
-import {ref} from "vue";
+import {onMounted, ref} from "vue";
 import {addDays, isYesterday} from "date-fns/esm";
+import * as echarts from 'echarts';
 
 export default {
   name: "StatisticPage",
   setup() {
+    const chartRef = ref();
+    const chartData = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line'
+        }
+      ]
+    };
+    const calendarData = {
+      "2023-4-9": {'new': 2, 'old': 5},
+      "2023-4-10": {'new': 3, 'old': 1},
+    }
+
+    onMounted(() => {
+      const myChart = echarts.init(chartRef.value);
+      myChart.setOption(chartData);
+    })
+    let aa = {year: 1, month: 2}
+
     return {
-      value: ref(addDays(Date.now(), 1).valueOf()),
+      value: ref(addDays(Date.now(), 1).valueOf()),//当前日期
+      chartRef,
+      calendarData,
       handleUpdateValue(_, {year, month, date}) {
       },
       isDateDisabled(timestamp) {
@@ -77,9 +114,12 @@ export default {
 
 <style scoped>
 .container {
-  background-color: #2c3e50;
   display: flex;
   align-items: center;
+}
+
+.chart {
+  height: 500px;
 }
 
 .calendar {
