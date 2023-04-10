75b1093d218e64b3c63f10bda73f39fe92be0d1e
lvyuanqiu <1246847701@qq.com>
2023-04-10 11:28:42 +0800

    feat: 聊天界面

---
+++ src/components/chatPage/ChatBox.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="chat_top">
+      <img src="../../assets/img/kaleidoBlank.png" height="170" width="150">
+    </div>
+    <ChatMessageRight time="12:00" content="啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦" ></ChatMessageRight>
+    <ChatMessageLeft time="12:02" content="啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦" ></ChatMessageLeft>
+    <ChatMessageRight time="12:00" content="啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦" ></ChatMessageRight>
+    <ChatMessageLeft time="12:02" content="啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦" ></ChatMessageLeft>
+    <ChatMessageRight time="12:00" content="啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦" ></ChatMessageRight>
+    <ChatMessageLeft time="12:02" content="啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦" ></ChatMessageLeft>
+  </div>
+</template>
+
+<script>
+import ChatMessageLeft from "@/components/chatPage/ChatMessageLeft";
+import ChatMessageRight from "@/components/chatPage/ChatMessageRight";
+export default {
+  name: "ChatBox",
+  components:{
+    ChatMessageRight,
+    ChatMessageLeft
+  }
+}
+</script>
+
+<style scoped>
+
+/* top */
+.chat_top{
+  width: 100%;
+  height: 50px;
+  position: absolute;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  padding-left: 20px;
+  font-size: 20px;
+  line-height: 50px;
+  box-sizing: border-box;
+  font-weight: 550;
+  border-width: 0px;
+}
+
+</style>
---
+++ src/components/chatPage/ChatPage.vue
@@ -0,0 +1,169 @@
+<template>
+  <div class="container">
+    <div class="main">
+      <!-- 整个注册盒子 -->
+      <div class="loginbox">
+        <!-- 左侧的注册盒子 -->
+        <div class="background">
+        </div>
+        <!-- 右侧的注册盒子 -->
+        <div v-show="0===number" class="content-login">
+          <NScrollbar class="chat" style="max-height: 500px">
+            <ChatBox></ChatBox>
+          </NScrollbar>
+          <div class="bottom">
+            <NInput class="message" round placeholder="Type a message...">
+            </NInput>
+            <NButton class="send" strong secondary type="info">
+              提问
+            </NButton>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+import {NInput , NButton, NScrollbar} from 'naive-ui';
+import {ref} from "vue";
+import ChatBox from "@/components/chatPage/ChatBox";
+export default {
+  components:{
+    NInput,
+    NButton,
+    NScrollbar,
+    ChatBox
+  },
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      number: 0,
+      message : "aaaaaa",
+      time : "12:00"
+    }
+  },
+  setup () {
+    return {
+      value: ref(null)
+    }
+  },
+
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.chat {
+  position: relative;
+}
+.send {
+  margin-left: 20px;
+}
+.message {
+  width: 400px;
+}
+.content-login .bottom {
+  position: absolute;
+  bottom: 120px;
+  display: flex;
+  justify-content: space-between;
+  color: #2A928F;
+  height: 40px;
+  font-size:40px
+}
+.content-login{
+  top:26%;
+  left:36.5%;
+  width: 600px;
+  height: 600px;
+  margin: auto;
+  background: rgba(223,219,219,0.2);
+  display: flex;
+  border-radius: 5px;
+  justify-content: center;
+  box-shadow: 0 25px 35px rgba(0,0,0,0.8);
+}
+
+.content-bottom :hover{
+  cursor: pointer;
+}
+
+.loginbox{
+  position:absolute;
+  width:1200px;
+  height:800px;
+  top:50%;
+  left:50%;
+  display:flex;
+  align-items: center;
+  transform:translate(-50%,-50%);
+  background-color: rgba(255,255,255,0.3);
+  box-shadow: 0 12px 16px 0  rgba(0,0,0,0.24), 0 17px 50px 0 #4E655D;
+}
+
+.loginbox .background{
+  width:400px;
+  height: 600px;
+  margin: auto;
+  background-image:url('../assets/img_1.png');
+  background-size:cover;
+  display: table-cell;
+  vertical-align: middle;
+  font-family:sans-serif;
+}
+
+input {
+  outline-style: none ;
+  border: 0;
+  border-bottom:2px solid #2A928F;
+  background-color:transparent;
+  height:20px;
+  font-family:sans-serif;
+  font-size:15px;
+  color:#445b53;
+  font-weight:bold;
+}
+
+input:focus{
+  border-bottom:2px solid #445b53;
+  background-color:transparent;
+  transition: all 0.2s ease-in;
+  font-family:sans-serif;
+  font-size:15px;
+  color:#445b53;
+  font-weight:bold;
+}
+input:hover{
+  border-bottom:2px solid #445b53;
+  background-color:transparent;
+  transition: all 0.2s ease-in;
+  font-family:sans-serif;
+  font-size:15px;
+  color:#445b53;
+  font-weight:bold;
+
+}
+
+input:-webkit-autofill {
+  /* 修改默认背景框的颜色 */
+  box-shadow: 0 0 0px 1000px  #89AB9E inset !important;
+  /* 修改默认字体的颜色 */
+  -webkit-text-fill-color: #445b53;
+}
+
+input:-webkit-autofill::first-line {
+  /* 修改默认字体的大小 */
+  font-size: 15px;
+  /* 修改默认字体的样式 */
+  font-weight:bold;
+}
+</style>
