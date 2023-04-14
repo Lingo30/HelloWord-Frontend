f349837bb992514c5098cb39fea7208f493f4346
王永瑶 <19374223@buaa.edu.cn>
2023-04-14 21:08:12 +0800

    feat: 限制标签字符

---
+++ src/components/userInfo/DynamicTags.vue
@@ -0,0 +1,43 @@
+<template>
+  <n-dynamic-tags
+      ref="dynamicTagsRef"
+      type="success"
+      v-model:value="model.value"
+      :max="10"
+      @input="onTagInput"
+  />
+</template>
+
+<script>
+import {ref} from "vue";
+
+export default {
+  name: "DynamicTags",
+  props: {
+    modelValue: Array,
+  },
+  setup(props, { emit }) {
+    const dynamicTagsRef = ref(null);
+    // 限制最大字符数为10
+    const maxLength = 10;
+
+    const onTagInput = (value) => {
+      const inputEl = dynamicTagsRef.value.$el.querySelector('input');
+      inputEl.maxLength = maxLength;
+      emit('update:modelValue', value);
+    };
+
+    return {
+      model: {
+        value: props.modelValue,
+      },
+      dynamicTagsRef,
+      onTagInput,
+    };
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
