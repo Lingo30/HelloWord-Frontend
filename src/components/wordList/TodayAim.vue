b3f6c099f4b0a526b77549019974c8e2eec70e22
lijinghao <lijinghao@buaa.edu.com>
2023-04-20 12:24:45 +0800

    fix: 修复更新每日背诵单词数请求过于频繁的bug

--- src/components/wordList/TodayAim.vue
+++ src/components/wordList/TodayAim.vue
@@ -1,10 +1,11 @@
 <template>
   <n-space class="container">
     <span style="font-weight: bold;margin-right: 10vh;margin-left: 25vh">今天的小目标</span>
-    <n-slider @update:value="onUpdate" v-model:value="target" :step="1" :marks="mark" :max="300" keyboard style="width: 80vh">
+    <n-slider @mousedown="pressFlag=true" v-model:value="target" :step="1" :marks="mark" :max="300" keyboard
+              style="width: 80vh">
       <template #thumb>
         <n-icon-wrapper :size="24" :border-radius="12">
-          <n-icon :size="18" :component="Accessibility" />
+          <n-icon :size="18" :component="Accessibility"/>
         </n-icon-wrapper>
       </template>
     </n-slider>
@@ -22,30 +23,41 @@ export default {
   setup() {
     const target = ref(0);
     const mark = ref({});
+    const pressFlag = ref(false)//是否在拖动进度条
     // mark.value[0] = '当前进度:' + 0
 
+    //添加鼠标抬起监听，更改每日背诵单词数
+    document.addEventListener('mouseup', () => {
+      if (pressFlag.value) {
+        pressFlag.value = false
+        //进行更新
+        update()
+      }
+    })
+
     async function load() {
-      getLearnedWords(store.state.user.uid).then((res)=>{
+      getLearnedWords(store.state.user.uid).then((res) => {
         const sum = res.sum
         mark.value[sum] = '当前进度：' + res.sum
       })
-      getAim(store.state.user.uid).then((res)=>{
+      getAim(store.state.user.uid).then((res) => {
         target.value = res.target
       })
     }
 
-    onBeforeMount(()=>{
+    onBeforeMount(() => {
       load();
     })
 
-    function onUpdate() {
+    //更新每日背诵单词数
+    function update() {
       // console.log(target.value);
-      setAim(store.state.user.uid,target.value);
+      setAim(store.state.user.uid, target.value);
     }
 
     return {
       mark,
-      onUpdate,
+      pressFlag,
       Accessibility,
       target
     };
