a12d69574c6484c2f8f9159a1fd232950314ae68
lvyuanqiu <1246847701@qq.com>
2023-04-21 16:24:24 +0800

    feat: 调整侧边栏式样

--- src/components/global/SideBar.vue
+++ src/components/global/SideBar.vue
@@ -3,60 +3,46 @@
     <ul>
       <li>
         <router-link labelTooltip="单词背诵"  to="/user/learn" class="s-sidebar__nav-link">
-          <br>
-          <n-icon size="25" :component="School" :depth="router.currentRoute.value.name!=='learn'?3:1" color="#ffffff"/>
-<!--          <br>-->
-<!--          <img v-if="router.currentRoute.value.name!=='learn'" class="a" src="../../assets/img/home.png" width="20"-->
-<!--               height="20">-->
-<!--          <img v-else class="a" src="../../assets/img/homeSelected.png" width="20" height="20">-->
+          <div class="box">
+            <n-icon style="top:50%; transform:translate(0,-50%);" size="4vh" :component="School" :depth="router.currentRoute.value.name!=='learn'?3:1" color="#ffffff"/>
+          </div>
         </router-link>
       </li>
       <li>
         <router-link labelTooltip="单词复习" to="/user/review" class="s-sidebar__nav-link">
-          <br>
-          <n-icon size="25" :component="Notifications" :depth="router.currentRoute.value.name!=='review'?3:1" color="#ffffff"/>
-<!--          <br>-->
-<!--          <img v-if="router.currentRoute.value.name!=='review'" class="a" src="../../assets/img/notification.png"-->
-<!--               width="20" height="20">-->
-<!--          <img v-else class="a" src="../../assets/img/notificationSelected.png" width="20" height="20">-->
+          <div class="box">
+            <n-icon style="top:50%; transform:translate(0,-50%);" size="4vh" :component="Notifications" :depth="router.currentRoute.value.name!=='review'?3:1" color="#ffffff"/>
+          </div>
         </router-link>
       </li>
       <li>
         <router-link labelTooltip="单词表" to="/user/wordlist" class="s-sidebar__nav-link">
-          <br>
-          <n-icon size="25" :component="Book" :depth="router.currentRoute.value.name!=='wordlist'?3:1" color="#ffffff"/>
-<!--          <br>-->
-<!--          <img v-if="router.currentRoute.value.name!=='wordlist'" class="a" src="../../assets/img/wordlist.png"-->
-<!--               width="20" height="20">-->
-<!--          <img v-else class="a" src="../../assets/img/wordlistSelected.png" width="20" height="20">-->
+          <div class="box">
+            <n-icon style="top:50%; transform:translate(0,-50%);" size="4vh" :component="Book" :depth="router.currentRoute.value.name!=='wordlist'?3:1" color="#ffffff"/>
+          </div>
         </router-link>
       </li>
       <li>
         <router-link labelTooltip="智能对话" to="/user/chat" class="s-sidebar__nav-link">
-          <br>
-          <n-icon size="25" :component="Chatbubble" :depth="router.currentRoute.value.name!=='chat'?3:1" color="#ffffff"/>
-<!--          <br>-->
-<!--          <img v-if="router.currentRoute.value.name!=='chat'" class="a" src="../../assets/img/talk.png" width="20"-->
-<!--               height="20">-->
-<!--          <img v-else class="a" src="../../assets/img/talkSelected.png" width="20" height="20">-->
+          <div class="box">
+            <n-icon style="top:50%; transform:translate(0,-50%);" size="4vh" :component="Chatbubble" :depth="router.currentRoute.value.name!=='chat'?3:1" color="#ffffff"/>
+          </div>
         </router-link>
       </li>
       <li>
         <router-link labelTooltip="产品介绍"  to="/user/help" class="s-sidebar__nav-link">
-          <br>
-          <n-icon size="25" :component="HelpCircle" :depth="router.currentRoute.value.name!=='help'?3:1" color="#ffffff"/>
-<!--          <br>-->
-<!--          <img v-if="router.currentRoute.value.name!=='statistic'" src="../../assets/img/chart.png" width="20"-->
-<!--               height="20">-->
-<!--          <img v-else class="a" src="../../assets/img/chartSelected.png" width="20" height="20">-->
+          <div class="box">
+            <n-icon style="top:50%; transform:translate(0,-50%);" size="4vh" :component="HelpCircle" :depth="router.currentRoute.value.name!=='statistic'?3:1" color="#ffffff"/>
+          </div>
         </router-link>
       </li>
     </ul>
     <!--    底部用户个人信息页面跳转-->
     <div class="bottom-button">
       <router-link labelTooltip="我的信息" to="/user/info" class="s-sidebar__nav-link">
-        <br>
-        <n-icon size="25" :component="Person" :depth="router.currentRoute.value.name!=='info'?3:1" color="#ffffff"/>
+        <div class="box">
+          <n-icon style="top:50%; transform:translate(0,-50%);" size="4vh" :component="Person" :depth="router.currentRoute.value.name!=='info'?3:1" color="#ffffff"/>
+        </div>
       </router-link>
     </div>
   </nav>
@@ -92,59 +78,62 @@ export default {
 <style scoped>
 .s-sidebar__nav {
   position: fixed;
-  top: 0;
-  /*overflow: hidden;*/
   transition: all .3s ease-in;
-  width: 4em;
   height: 100%;
   background: #26A474;
+  width: 5%;
+  left: 0;
   color: rgba(255, 255, 255, 0.7);
 }
 
 .s-sidebar__nav ul {
   position: absolute;
-  top: 2em;
-  margin: 0;
   padding: 0;
-  width: 8em;
+  height: 80%;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+  margin-top: 30%;
 }
 
 .s-sidebar__nav ul li {
-  width: 50%;
+  width: 100%;
+  height: 10%;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
 }
 
-br {
-  display: block; /* makes it have a width */
-  content: ""; /* clears default height */
-  margin-top: 20px; /* todo 调整br大小 将图片居中 */
-}
 
 .s-sidebar__nav-link {
   position: relative;
   display: inline-block;
   width: 80%;
-  height: 4em;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+  height: 100%;
+
 }
 
 .s-sidebar__nav-link:hover {
   background-color: rgba(255, 255, 255, 0.5) /*选中时的颜色*/;
 }
 
-@media (min-width: 42em) {
-  /* Sidebar */
-  .s-sidebar__nav {
-    width: 4em;
-    left: 0;
-  }
+.box{
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
 }
-
 .bottom-button {
   /*background-color: black;*/
   position: absolute;
-  bottom: 2em;
-  margin: 0;
-  padding: 0;
-  width: 4em;
+  bottom: 10%;
+  height: 8%;
+  width: 100%;
 }
 
 .s-sidebar__nav-link:hover:after {
