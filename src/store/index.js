4efaae019538f1bc2e723ad66178278b0f10ccb8
徐子航 <20373284@buaa.edu.cn>
2023-04-24 17:34:41 +0800

    fix: 单词复习界面样式调整

--- src/store/index.js
+++ src/store/index.js
@@ -12,6 +12,7 @@ export default createStore({
             groupWords: [
                 {
                     word: "",
+                    phonetic_symbol: "",
                     definition_cn: "",
                 }
             ],
