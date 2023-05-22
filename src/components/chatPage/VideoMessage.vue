e1db0e7fcf6ab3ddf1e5d35c86d3b40e2d95478d
lvyuanqiu <1246847701@qq.com>
2023-05-23 00:59:14 +0800

    feat: 智能语音

---
+++ src/components/chatPage/VideoMessage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div >
+    <VideoMessageLeft v-show="true === type" v-bind:time=time v-bind:content=content></VideoMessageLeft>
+    <VideoMessageRight v-show="false === type" v-bind:time=time v-bind:content=content></VideoMessageRight>
+  </div>
+</template>
+
+<script>
+import VideoMessageLeft from "@/components/chatPage/VideoMessageLeft";
+import VideoMessageRight from "@/components/chatPage/VideoMessageRight";
+export default {
+  name: "VideoMessage",
+  components:{
+    VideoMessageRight,
+    VideoMessageLeft,
+  },
+  props:{
+    type:{
+      type: Boolean,
+      default: false
+    },
+    time:{
+      type: String,
+      default: '--:--'
+    },
+    content:{ // 怎么播放音频  todo
+      type: String,
+      default: '啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦啦'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
